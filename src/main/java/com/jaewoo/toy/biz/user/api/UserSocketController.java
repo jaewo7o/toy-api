38fdafd91895d66d59776dfa9da24f376f64c441
JAEWOO <jaewo7o@naver.com>
2020-12-25 18:50:25 +0900

    websocket

--- src/main/java/com/jaewoo/toy/biz/user/api/UserSocketController.java
+++ src/main/java/com/jaewoo/toy/biz/user/api/UserSocketController.java
@@ -1,4 +1,30 @@
 package com.jaewoo.toy.biz.user.api;
 
+import com.jaewoo.toy.biz.user.domain.vo.SocketUserVO;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserSocketController {
+
+    // /receive를 메시지를 받을 endpoint로 설정합니다.
+    @MessageMapping("/receive")
+
+    // /send로 메시지를 반환합니다.
+    @SendTo("/send")
+
+    // SocketHandler는 1) /receive에서 메시지를 받고, /send로 메시지를 보내줍니다.
+    // 정의한 SocketVO를 1) 인자값, 2) 반환값으로 사용합니다.
+    public SocketUserVO SocketHandler(SocketUserVO socketVO) {
+        // vo에서 getter로 userName을 가져옵니다.
+        String userName = socketVO.getUserName();
+        // vo에서 setter로 content를 가져옵니다.
+        String content = socketVO.getContent();
+
+        // 생성자로 반환값을 생성합니다.
+        SocketUserVO result = new SocketUserVO(userName, content);
+        // 반환
+        return result;
+    }
 }
--- src/main/java/com/jaewoo/toy/biz/user/domain/vo/SocketUserVO.java
+++ src/main/java/com/jaewoo/toy/biz/user/domain/vo/SocketUserVO.java
@@ -1,2 +1,13 @@
-package com.jaewoo.toy.biz.user.domain.vo;public class SocketUserVO {
+package com.jaewoo.toy.biz.user.domain.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class SocketUserVO {
+    private String userName;
+    private String content;
 }
